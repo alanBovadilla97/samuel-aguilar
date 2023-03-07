2b0616ee13f874c15e12af503c1fcf1f10db793b
Alan Bovadilla <alanBovadilla97@gmail.com>
2023-03-06 20:50:31 -0600

    Add Hash Router

--- src/index.js
+++ src/index.js
@@ -3,18 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Footer from './components/footer/Footer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <App />
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
 
