b988c3fefc6ab2ed3955836742d799f34af1b244
Alan Bovadilla <alan.bovadilla@levelupdevelopment.com>
2026-02-27 21:54:53 -0600

    Refactor SocialNetworks component: update layout to include title and adjust direction for improved presentation

--- src/components/footer/SocialNetworks.js
+++ src/components/footer/SocialNetworks.js
@@ -1,12 +1,17 @@
-import { Stack } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import SocialNetworkItem from "./SocialNetworkItems";
 
 export default function SocialNetworks({ items }) {
   return(
-    <Stack alignItems="center" justifyContent="center" gap={2} direction='row' py={1}>
-      {items.map(sns => (
-        <SocialNetworkItem sns={sns} />
-      ))}
+    <Stack alignItems="center" justifyContent="center" gap={2} direction='column' py={1}>
+      <Typography variant="body1" color="common.white">
+        Síganos
+      </Typography>
+      <Stack direction='row' gap={2}>
+        {items.map(sns => (
+          <SocialNetworkItem sns={sns} />
+        ))}
+      </Stack>
     </Stack>
   );
 };
